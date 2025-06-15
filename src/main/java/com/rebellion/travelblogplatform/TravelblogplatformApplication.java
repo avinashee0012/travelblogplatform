1a197d75abeddb1e779e2299d574acb1e661d99b
Rebellion <avinashee0012@gmail.com>
2025-06-15 23:23:58 +0530

    Initialized project

---
+++ src/main/java/com/rebellion/travelblogplatform/TravelblogplatformApplication.java
@@ -0,0 +1,13 @@
+package com.rebellion.travelblogplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelblogplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelblogplatformApplication.class, args);
+	}
+
+}
