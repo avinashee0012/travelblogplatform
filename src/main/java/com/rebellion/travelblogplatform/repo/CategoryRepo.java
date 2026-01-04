8bf0ce7982c77b568f9a02db222fea9c35abb14f
Rebellion <avinashee0012@gmail.com>
2026-01-05 02:11:43 +0530

    Fixed incorrect import

--- src/main/java/com/rebellion/travelblogplatform/repo/CategoryRepo.java
+++ src/main/java/com/rebellion/travelblogplatform/repo/CategoryRepo.java
@@ -1,9 +1,9 @@
 package com.rebellion.travelblogplatform.repo;
 
-import java.util.Locale.Category;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.rebellion.travelblogplatform.entity.Category;
 
 @Repository
 public interface CategoryRepo extends JpaRepository<Category, Long>{
