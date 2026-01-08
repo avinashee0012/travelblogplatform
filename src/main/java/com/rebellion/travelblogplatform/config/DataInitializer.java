4810294c5881b99a7492b785b8a7558f16fc7907
Rebellion <avinashee0012@gmail.com>
2026-01-08 23:34:08 +0530

    Fixed role and user initializer

--- src/main/java/com/rebellion/travelblogplatform/config/DataInitializer.java
+++ src/main/java/com/rebellion/travelblogplatform/config/DataInitializer.java
@@ -3,17 +3,22 @@ package com.rebellion.travelblogplatform.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.rebellion.travelblogplatform.dto.User.UserRegisterDto;
 import com.rebellion.travelblogplatform.entity.Role;
 import com.rebellion.travelblogplatform.entity.User;
 import com.rebellion.travelblogplatform.repo.RoleRepo;
 import com.rebellion.travelblogplatform.repo.UserRepo;
+import com.rebellion.travelblogplatform.service.UserService;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Configuration
 public class DataInitializer {
 
     @Bean
-    CommandLineRunner initRoles(UserRepo userRepo, RoleRepo roleRepo) {
+    CommandLineRunner initRoles(UserService userService, UserRepo userRepo, RoleRepo roleRepo, PasswordEncoder passwordEncoder) {
         return args -> {
             Role userRole = new Role("USER", "Default role with least authority");
             Role authorRole = new Role("AUTHOR", "Role assigned to an author of blog");
@@ -23,12 +28,16 @@ public class DataInitializer {
             roleRepo.save(authorRole);
             roleRepo.save(adminRole);
 
-            User user = new User("user", "user@email.com", "userPassword", userRole);
-            User author = new User("author", "author@email.com", "authorPassword", authorRole);
-            User admin = new User("admin", "admin@email.com", "adminPassword", adminRole);
+            userService.register(new UserRegisterDto("user", "user@email.com", "userPassword"));
 
-            userRepo.save(user);
+            userService.register(new UserRegisterDto("author", "author@email.com", "authorPassword"));
+            User author = userRepo.findByEmail("author@email.com").orElseThrow(() -> new EntityNotFoundException());
+            author.changeRole(authorRole);
             userRepo.save(author);
+
+            userService.register(new UserRegisterDto("admin", "admin@email.com", "adminPassword"));
+            User admin = userRepo.findByEmail("admin@email.com").orElseThrow(() -> new EntityNotFoundException());
+            admin.changeRole(adminRole);
             userRepo.save(admin);
         };
     }
