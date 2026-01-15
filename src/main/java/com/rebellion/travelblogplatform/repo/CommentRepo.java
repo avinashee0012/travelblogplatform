6c833c593602322a4d9b939c24e16196ea0d9226
Rebellion <avinashee0012@gmail.com>
2026-01-15 23:17:13 +0530

    Update comments to non-paginated response

--- src/main/java/com/rebellion/travelblogplatform/repo/CommentRepo.java
+++ src/main/java/com/rebellion/travelblogplatform/repo/CommentRepo.java
@@ -1,9 +1,7 @@
 package com.rebellion.travelblogplatform.repo;
 
-import java.util.Optional;
+import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,7 +9,5 @@ import com.rebellion.travelblogplatform.entity.Comment;
 
 @Repository
 public interface CommentRepo extends JpaRepository<Comment, Long>{
-    Page<Comment> findByBlogIdOrderByCreatedAtDesc(Long blogId, Pageable pageable);
-    @SuppressWarnings("null")
-    Optional<Comment> findById(Long commentId);
+    List<Comment> findByBlogIdOrderByCreatedAtDesc(Long blogId);
 }
