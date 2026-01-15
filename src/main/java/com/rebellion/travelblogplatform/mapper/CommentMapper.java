bbb6d4169c67000b1cc93b2e6d93782275ec732d
Rebellion <avinashee0012@gmail.com>
2026-01-15 23:13:05 +0530

    Implemented create comment method

--- src/main/java/com/rebellion/travelblogplatform/mapper/CommentMapper.java
+++ src/main/java/com/rebellion/travelblogplatform/mapper/CommentMapper.java
@@ -2,16 +2,16 @@ package com.rebellion.travelblogplatform.mapper;
 
 import com.rebellion.travelblogplatform.dto.Comment.CommentRequestDto;
 import com.rebellion.travelblogplatform.dto.Comment.CommentResponseDto;
+import com.rebellion.travelblogplatform.entity.Blog;
 import com.rebellion.travelblogplatform.entity.Comment;
+import com.rebellion.travelblogplatform.entity.User;
 
 public class CommentMapper {
     public static CommentResponseDto toResponse(Comment comment){
-        // TODO CommentMapper::toResponse
-        return new CommentResponseDto(null, null, null, null);
+        return new CommentResponseDto(comment.getId(), comment.getContent(), comment.getCommentor().getUsername(), comment.getCreatedAt());
     }
 
-    public static Comment toEntity(CommentRequestDto commentRequestDto){
-        // TODO CommentMapper::toEntity
-        return new Comment(null, null, null);
+    public static Comment toEntity(CommentRequestDto commentRequestDto, Blog blog, User user){
+        return new Comment(user, blog, commentRequestDto.getContent());
     }
 }
--- src/main/java/com/rebellion/travelblogplatform/service/impl/CommentServiceImpl.java
+++ src/main/java/com/rebellion/travelblogplatform/service/impl/CommentServiceImpl.java
@@ -3,24 +3,41 @@ package com.rebellion.travelblogplatform.service.impl;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import com.rebellion.travelblogplatform.config.util.SecurityUtil;
 import com.rebellion.travelblogplatform.dto.Comment.CommentRequestDto;
 import com.rebellion.travelblogplatform.dto.Comment.CommentResponseDto;
+import com.rebellion.travelblogplatform.entity.Blog;
 import com.rebellion.travelblogplatform.entity.Comment;
+import com.rebellion.travelblogplatform.entity.User;
+import com.rebellion.travelblogplatform.exception.NotLoggedInException;
+import com.rebellion.travelblogplatform.mapper.CommentMapper;
+import com.rebellion.travelblogplatform.repo.BlogRepo;
 import com.rebellion.travelblogplatform.repo.CommentRepo;
+import com.rebellion.travelblogplatform.repo.UserRepo;
 import com.rebellion.travelblogplatform.service.CommentService;
 
 @Service
 public class CommentServiceImpl implements CommentService{
     private final CommentRepo commentRepo;
+    private final UserRepo userRepo;
+    private final BlogRepo blogRepo;
 
-    public CommentServiceImpl(CommentRepo commentRepo) {
+    public CommentServiceImpl(CommentRepo commentRepo, UserRepo userRepo, BlogRepo blogRepo) {
         this.commentRepo = commentRepo;
+        this.userRepo = userRepo;
+        this.blogRepo = blogRepo;
     }
 
     @Override
     public CommentResponseDto createComment(Long blogId, CommentRequestDto commentRequestDto) {
-        // TODO createComment(Long blogId, CommentRequestDto commentRequestDto)
-        throw new UnsupportedOperationException("Unimplemented method 'createComment'");
+        User user = userRepo.findByEmail(SecurityUtil.getCurrentUserEmail()).orElseThrow(() -> new NotLoggedInException());
+        Blog blog = null;
+        if(blogId != null)
+            blogRepo.findById(blogId).orElseThrow(() -> new IllegalArgumentException("Invalid blog id"));
+        Comment comment = CommentMapper.toEntity(commentRequestDto, blog, user);
+        if(comment != null) 
+            commentRepo.save(comment);
+        return CommentMapper.toResponse(comment);
     }
 
     @Override
