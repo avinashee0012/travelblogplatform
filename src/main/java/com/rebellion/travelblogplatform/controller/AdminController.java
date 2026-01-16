a159183654aef57de4a64605957152bda69d45c1
Rebellion <avinashee0012@gmail.com>
2026-01-16 23:11:43 +0530

    Initialized Admin endpoints

---
+++ src/main/java/com/rebellion/travelblogplatform/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.rebellion.travelblogplatform.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admins/")
+public class AdminController {
+    
+    // manage users
+    // manage blogs
+    // manage comments
+}
