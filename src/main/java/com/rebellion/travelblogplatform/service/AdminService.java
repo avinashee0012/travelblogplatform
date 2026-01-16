97cc3b74ebe7738db8f2df1965050bcfd0b20797
Rebellion <avinashee0012@gmail.com>
2026-01-16 23:13:39 +0530

    Initialized Admin service

---
+++ src/main/java/com/rebellion/travelblogplatform/service/AdminService.java
@@ -0,0 +1,7 @@
+package com.rebellion.travelblogplatform.service;
+
+public interface AdminService {
+    // manage users
+    // manage blogs
+    // manage comments
+}
---
+++ src/main/java/com/rebellion/travelblogplatform/service/impl/AdminServiceImpl.java
@@ -0,0 +1,10 @@
+package com.rebellion.travelblogplatform.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.rebellion.travelblogplatform.service.AdminService;
+
+@Service
+public class AdminServiceImpl implements AdminService{
+    
+}
