03494ac7115c194d5e7a27ae7a62b5d816d95bf1
avinashee0012 <avinashee0012@gmail.com>
2026-01-23 19:54:46 +0530

    add /api/public endpoint to securityConfig permit all

--- src/main/java/com/rebellion/travelblogplatform/controller/BlogController.java
+++ src/main/java/com/rebellion/travelblogplatform/controller/BlogController.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -46,7 +47,7 @@ public class BlogController {
     }
 
     @PreAuthorize("hasRole('AUTHOR')")
-    @PutMapping("/{id}/publish")
+    @PatchMapping("/{id}/publish")
     public ResponseEntity<BlogResponseDto> publishBlog(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(blogService.publishBlog(id));
     }
