2e30e03e6d6bce573a32c98607d0fd21e4224f8c
avinashee0012 <avinashee0012@gmail.com>
2026-01-23 19:32:04 +0530

    Make author profile require authentication

--- src/main/java/com/rebellion/travelblogplatform/controller/UserController.java
+++ src/main/java/com/rebellion/travelblogplatform/controller/UserController.java
@@ -2,6 +2,7 @@ package com.rebellion.travelblogplatform.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{username}")
     public ResponseEntity<AuthorProfileResponseDto> getUserProfile(@PathVariable String username){
         return ResponseEntity.status(HttpStatus.OK).body(userService.getAuthorProfile(username));
