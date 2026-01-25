a3cf9f62f58343b20ffb15e80dfce935a18eb06e
avinashee0012 <avinashee0012@gmail.com>
2026-01-25 11:15:07 +0530

    add paginated findByTitle method to blogrepo

--- src/main/java/com/rebellion/travelblogplatform/repo/BlogRepo.java
+++ src/main/java/com/rebellion/travelblogplatform/repo/BlogRepo.java
@@ -3,6 +3,8 @@ package com.rebellion.travelblogplatform.repo;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,5 @@ import com.rebellion.travelblogplatform.enums.Status;
 public interface BlogRepo extends JpaRepository<Blog, Long>{
     Optional<Blog> findBySlug(String slug);
     List<Blog> findTop10ByAuthorUsernameAndStatusOrderByUpdatedAtDesc(String username, Status status);
+    Page<Blog> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
 }
